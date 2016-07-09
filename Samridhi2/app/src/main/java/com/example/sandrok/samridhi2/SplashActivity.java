b96a3f910eb392041b725384bb3e1a498cbb54ac
sandyrok <sandeepreddy.rokkam@gmail.com>
2016-07-09 19:11:43 +0530

    second app

---
+++ Samridhi2/app/src/main/java/com/example/sandrok/samridhi2/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.sandrok.samridhi2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    Animation animFadein;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView=(ImageView)findViewById(R.id.logo);
+
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fadein);
+        imageView.startAnimation(animFadein );
+    }
+}
