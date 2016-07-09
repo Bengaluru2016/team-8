61c62d6d0bcce9aa286161c44be1eff73e8e1f72
Rohan Kumar <rohan.carmel@gmail.com>
2016-07-10 04:06:10 +0530

    created marks Activity

---
+++ Samridhi2/app/src/main/java/com/example/sandrok/samridhi2/MarksActivity.java
@@ -0,0 +1,13 @@
+package com.example.sandrok.samridhi2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marks);
+    }
+}
